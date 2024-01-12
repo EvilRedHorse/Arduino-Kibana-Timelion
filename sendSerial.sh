c5a6ab51b62757292ba356c6ec257aada3478bee
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2024-01-11 20:10:17 -0500

    Update sendSerial.sh
    Improved readability & commenting.
    TTY session is reliable.
    Variables are accessible.

--- sendSerial.sh
+++ sendSerial.sh
@@ -1,32 +1,48 @@
 #! /bin/bash --posix
 
-# The script is used to send timeline series sensor data from Arduino to Elasticsearch
+# The script is used to send sensor data from Arduino to Elasticsearch for Kibana/Timelion.
 
+# allow read/write on dev/shm
+# sudo chmod u+rw /dev/shm
+
+# logfile timestamp
+TIME=$(date +%s%N)
 # logfile to use on /dev/shm
-LOGFILE=/dev/shm/report.json
-# this is the location to insert your Arduino password
-ARDUINO_PASS=3fb77e036133e2fd69f8510d35ca86342cd2b9872594bbf4645e378f8c4a2b3e27700c75a6ac228278193f129bef2cf47ba03f08a1e93656a939491ca26ece50
-# this is the location to insert your base64 Elasticsearch password
-ELASTIC_PASS=
-# this is the location to insert your Elasticsearch hostname
-ELASTIC_HOST=
+LOGFILE=/dev/shm/report$TIME.json
 
-# start screen session
-sudo -u $USER screen  -dmSL serialReport  -Logfile $LOGFILE socat stdio /dev/ttyACM0,b57600,raw,crnl
-# clear log now, before running commands
-rm $LOGFILE
-# recreate logfile
-touch $LOGFILE
-# runReport command, as user, in background
-sudo -u $USER echo "{\"getReport\": \"$ARDUINO_PASS\"}" > /dev/ttyACM0 &
-#sudo -u smc ./runReport.sh &
-# sleep 10 <- an interval to allow time between runReport(s)   
-sleep 10
-# output screenlog.0 with the json we need.
+# TTY connection/session info 
+TTY=/dev/ttyACM0
+BAUD=2400
+TTY_SESSION_SEC=60
+LOGFILE=/dev/shm/report.json
+# Arduino password
+ARDUINO_PASS=
+# Elasticsearch password
+ELASTIC_PASS=
+# Elasticsearch Host, Port & Cert directory.
+ELASTIC_HOST=127.0.0.1
+ELASTIC_PORT=9200
+CERT_DIR=~/.certs/http_ca.crt
+
+tty () {
+exec 4<$TTY 5>$TTY
+stty -F $TTY $BAUD -echo
+echo "{\"pass\": \"$ARDUINO_PASS\"}" > $TTY >&5 &
+read -t $TTY_SESSION_SEC -e REPLY <&4
+echo "$REPLY" > $LOGFILE
+}
+
+while [[ -z "${REPLY}" ]]; do
+    tty
+    sleep 3
+done
+
+# show logfile
 cat $LOGFILE
+
 # send json timeseries data to elasticsearch
-sudo -u $USER curl --cacert ~/.certs/http_ca.crt -u  elastic:$ELASTIC_PASS -XPOST https://$ELASTIC_HOST:9200/my-data-stream/_doc -H "Accept: application/json" -H "Content-Type:application/json" --data $(cat $LOGFILE)
-# close the screen session
-screen -S serialReport -X quit
-# remove the logfie
+sudo -u $USER curl --cacert $CERT_DIR -u elastic:$ELASTIC_PASS  -XPOST https://$ELASTIC_HOST:$ELASTIC_PORT/my-data-stream/_doc -H "Accept: application/json" -H "Content-Type:application/json" --data $(cat $LOGFILE)
+
+# remove the logfile
 rm $LOGFILE
+exit 0
