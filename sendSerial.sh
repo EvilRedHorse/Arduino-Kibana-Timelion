1db29adc3c9b96f56c834b032284b40fe4256373
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2024-06-13 23:17:13 -0400

    Update sendSerial.sh
    Silenced curl: `curl -s`
    Changed counter positions: Single command sent to Arduino.

--- sendSerial.sh
+++ sendSerial.sh
@@ -6,14 +6,15 @@
 # sudo chmod u+rw /dev/shm
 
 # logfile timestamp
-readonly TIME=$(date +%s%N)
+readonly TIME=$(date +%s+%N)
+
 # logfile to use on /dev/shm
 LOGFILE=/dev/shm/report$TIME.json
 
 # TTY connection/session info
 TTY=/dev/ttyACM0
 BAUD=2400
-TTY_SESSION_SEC=65
+TTY_SESSION_SEC=60
 
 # Arduino password
 ARDUINO_PASS=""
@@ -27,26 +28,34 @@ CERT_DIR=~/.certs/http_ca.crt
 counter=0
 
 tty () {
-    exec 4<$TTY 5>$TTY
-    stty -F $TTY $BAUD -echo
-    sleep 1
+
     if [[ $counter -lt 1 ]]; then
+        #echo "direction setup"
+        exec 4<$TTY 5>$TTY
+        #echo "stty setup"
+        stty -F $TTY $BAUD -echo
+        #eleep briefly after setup.    
+        sleep 3    
+        #echo "sending command to Arduino"
         echo "{\"pass\": \"$ARDUINO_PASS\"}" > $TTY >&5 &
     fi
+
+    #echo "reading"
+    read -t $TTY_SESSION_SEC -e REPLY <&4   
+
 }
 
-while [[ -z "${REPLY}" ]] && [[ $counter -lt 2 ]]; do
+while [[ -z "${REPLY}" ]] && [[ $counter -lt 3 ]]; do
     tty
-    read -t $TTY_SESSION_SEC -e REPLY <&4
     let "counter++"
-    done
+done
 
 echo "$REPLY" > $LOGFILE
 # show logfile
 cat $LOGFILE
 
 # send json timeseries data to elasticsearch
-sudo -u $USER curl --cacert $CERT_DIR -u elastic:$ELASTIC_PASS  -XPOST https://$ELASTIC_HOST:$ELASTIC_PORT/my-data-stream/_doc -H "Accept: application/json" -H "Content-Type:application/json" --data $(cat $LOGFILE)
+sudo -u $USER curl -s --cacert $CERT_DIR -u elastic:$ELASTIC_PASS  -XPOST https://$ELASTIC_HOST:$ELASTIC_PORT/my-data-stream/_doc -H "Accept: application/json" -H "Content-Type:application/json" --data $(cat $LOGFILE)
 
 # remove the logfile
 rm $LOGFILE
